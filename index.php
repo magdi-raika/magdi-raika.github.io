5b2b4bf5cee527e2b07d65be618915e38ec9b3c9
magdi-raika <magdiali00@gmail.com>
2020-02-05 23:41:34 +0100

    hello

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>salut</title>
+	<title>oppp</title>
 </head>
 <body>
 
+
+
 <center>
 	<table border="1" cellpadding="3">
-		<form method="POST" action="kkk.php">
+		<form method="POST" action="index.php">
 			<tr><td>numero:</td><td><input type="text" name="1" value=""></td></tr>
 			<tr><td><select name="signe" size="4">
 			<option value="+" selected>+</option>
@@ -23,5 +25,34 @@
 
 
 </center>
+
+
+<?php 
+
+if(!empty($_POST['1'])&&!empty($_POST['2'])){
+?>
+<h1>kabch, le resultat est :  
+<?php 
+$n1=$_POST['1'];
+	$n2=$_POST['2'];
+	
+
+$op=$_POST['signe'];
+if ($op=='+') {
+	echo $n1+$n2;
+}
+elseif ($op=='-') {
+	echo $n1-$n2;
+}
+elseif ($op=='*') {
+	echo $n1*$n2;
+}
+else{
+	echo $n1/$n2;
+}
+}
+
+?>
+</h1>
 </body>
 </html>
